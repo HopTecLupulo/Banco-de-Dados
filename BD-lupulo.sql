145b6e66554c027187dce9980723995f00749c4b
LccsBueno <luccas.lima@sptech.school>
2023-03-10 14:18:07 -0300

    Alterei alguns nomes de empresa e lúpulo

--- BD-lupulo.sql
+++ BD-lupulo.sql
@@ -11,9 +11,9 @@ INSERT INTO Lupulo VALUES
     (null, 'Cascade', 13),
     (null, 'Galaxy', 14),
     (null, 'Amarillo', 15),
-	(null, 'Alberto', 14),
-    (null, 'Supinpa', 12),
-    (null, 'Josoares', 14);
+	(null, 'Supinpa', 14),
+    (null, 'jiraya', 12),
+    (null, 'Jimbo', 14);
 
 CREATE TABLE Cadastro (
 id_cliente int primary key auto_increment,
@@ -33,11 +33,11 @@ INSERT INTO Cadastro VALUES
 	(null, '12.345.678/0001-00',null , 'AFGC Produtores de Lúpulo Ltda.', 'afgc@produtores.com', '1234-0001', '12345-000', 'Av. Xampons Xamps, 123', 'SP'),
     (null, '12.345.678/0001-01',null , 'Sayajins Lúpulos Inc.', 'sayalupulos@dbzinc.com', '1234-0002', '12345-001', 'Av. Sayajins, 456', 'MG'),
     (null, '12.345.678-0001-02',null , 'Obiwan Kenobi Lúpulos e Produção Ltda.', 'obiwan@lupuloprod.com', '1234-0003', '12345-002','Av. Baby Yoda, 789', 'SP'),
-    (null, '15.848.984-0001-01',null , 'Robertinho', 'robertinho@gmail.com', '1354-4772', '5143-5576', 'AV pamonha', 'MT'), 
-    (null, '12.098.984-0001-01',null , 'Larinsinha123', 'larisinha123@gmail.com', '8187-7821', '8756-9856', 'Av Rodrigues', 'SP'),
-    (null, '65.568.984-0001-01',null , 'Devolve meus filhos Ana	', 'ana@gmail.com', '8752-9875', '9813-1249', 'Av Nostalgia', 'PA'),
-    (null, '41.848.124-0001-01',null , 'Só vapo enterprises', 'vapo@gmail.com', '5786-2158', '8753-6524', 'Av beirut', 'AM'),
-    (null, '12.541.984-0001-01',null , 'Lorenzeti', 'lorenzeti@gmail.com', '5887-6371', '8754-9875', 'Av Gonzaga', 'PI');
+    (null, '15.848.984-0001-01',null , 'Empresa1', 'email1@gmail.com', '1354-4772', '5143-5576', 'AV pamonha', 'MT'), 
+    (null, '12.098.984-0001-01',null , 'Empresa2', 'email2@gmail.com', '8187-7821', '8756-9856', 'Av Rodrigues', 'SP'),
+    (null, '65.568.984-0001-01',null , 'Empresa3', 'email3@gmail.com', '8752-9875', '9813-1249', 'Av Nostalgia', 'PA'),
+    (null, '41.848.124-0001-01',null , 'Empresa4', 'email4@gmail.com', '5786-2158', '8753-6524', 'Av beirut', 'AM'),
+    (null, '12.541.984-0001-01',null , 'Empresa5', 'email5@gmail.com', '5887-6371', '8754-9875', 'Av Gonzaga', 'PI');
     
 CREATE TABLE Sensor (
 id_sensor int primary key auto_increment,
@@ -51,8 +51,8 @@ INSERT INTO Sensor VALUES
     (null, '003', 'Ativo'),
 	(null, '004', 'Ativo'),
     (null, '005', 'Inativo'),
-    (null,'006', 'Ativo'),
-    (null,'007','Ativo');
+    (null, '006', 'Ativo'),
+    (null, '007','Ativo');
     
 CREATE TABLE Dados (
 id_dados int primary key auto_increment,
